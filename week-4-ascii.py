a0e56bf9d09ac6758082d3d602fa15e18e201257
Anand_Swaroop <prashu.swaroop@gmail.com>
2018-09-24 02:12:25 +0530

    Assignment#2 WIP

--- week-4-ascii.py
+++ week-4-ascii.py
@@ -113,3 +113,29 @@
 # for span in spans:
 #     numbers.append(int(span.string))
 # print(sum(numbers))    
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter: ')
+link_line = int(input("Enter position: ")) - 1 
+# relative to first link
+count = int(input("Enter count: "))
+
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+while count >= 0:
+   html = urllib.request.urlopen(url, context=ctx).read()
+   soup = BeautifulSoup(html, 'html.parser')
+   tags = soup('a')
+   print(url)
+   url = tags[link_line].get("href", None)
+   count = count - 1
+
+
