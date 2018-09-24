b0d0febccb083a549b5e280cefb92a3147cf90f4
payne <prashu.swaroop@email.com>
2018-09-24 21:10:35 +0530

    Finished all the assignments of course 3

--- 6-week6-json.py
+++ 6-week6-json.py
@@ -132,3 +132,51 @@
 #         "consumer_secret" : "dNKenAC3New...mmn7Q",
 #         "token_key" : "10185562-eibxCp9n2...P4GEQQOSGI",
 #         "token_secret" : "H0ycCFemmC4wyf1...qoIpBo" }
+
+# Assignment - Extracting data from json
+# import urllib.request as ur
+# import json
+# # json_url = 'http://py4e-data.dr-chuck.net/comments_133482.json'
+# json_url = input("Enter location: ")
+# print("Retrieving ", json_url)
+# data = ur.urlopen(json_url).read().decode('utf-8')
+# print('Retrieved', len(data), 'characters')
+# json_obj = json.loads(data)
+# sum = 0
+# total_number = 0
+# for comment in json_obj["comments"]:
+#     sum += int(comment["count"])
+#     total_number += 1
+# print('Count:', total_number)
+# print('Sum:', sum)
+
+# Assignment - Geocoding using Google API
+
+# import urllib.request as ur
+# import urllib.parse as up
+# import json
+
+# serviceurl = "http://python-data.dr-chuck.net/geojson?"
+# # This API only accepts the university in a list of its accepted ones.
+# # This API uses the same parameters (sensor and address) as the Google API.
+# # This API also has no rate limit so you can test as often as you like.
+# # If you visit the URL with no parameters, you get a list of all of the address values which can be used with this API.
+
+# address_input = input("Enter location: ")
+# params = {"sensor": "false", "address": address_input}
+# url = serviceurl + up.urlencode(params)
+# print("Retrieving ", url)
+# data = ur.urlopen(url).read().decode('utf-8')
+# print('Retrieved', len(data), 'characters')
+# json_obj = json.loads(data)
+
+# place_id = json_obj["results"][0]["place_id"]
+# print("Place id", place_id)
+
+# Sample Execution:
+# C:\Users\anand\Personal_Git_Repos\python-course-3-web-data\python-course-3-web-data>python 6-week6-json.pyEnter location: Georgetown University Law Center
+# Retrieving  http://python-data.dr-chuck.net/geojson?sensor=false&address=Georgetown+University+Law+Center
+# Retrieved 2019 characters
+# Place id ChIJzRC3Ga6Hs4kRYhWPTMBbXww
+
+
